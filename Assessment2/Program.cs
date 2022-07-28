b885f74b147982ac2228c12b190432a24af5545e
MichaelDSwopeVox <digitalwhiterabbithole@gmail.com>
2022-07-27 20:56:03 -0400

    first commit Assessment2

---
+++ Assessment2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assesswment2
+{
+
+
+
+    class program
+
+    //method named CreateStringArray()... takes 3 strings as parameters..method should make new string[] ..next method should add
+    //each string into ist own spot in the string[] and finally return the string[].
+    static void Main(string[] args)
+    {
+        
+       static void CreateStringArray()
+       {
+       string[] strings = new string[] { "1", "2", "3" };
+
+
+            foreach (string CreateStringArray) 
+            {
+                CreateStringArray.Add();
+
+
+
+
+            }
+
+
+
+            //create static method named SumArray(int[]). method should add all the values in array together and return the sum
+            static string SumArray(int[] ) 
+            {
+
+                int[] arr = new int[] { 1, 2, 3 };
+                int sum = arr.Sum();
+                Console.WriteLine(sum);
+            }
+            // create the static method named RemoveNum(int List, int num)..check list Contains number parameter passed into method. if so remove
+            //Value from the List passed in return void
+            //return (void);
+            List<int> numList = new List<int>();
+            numList.Add(32);
+            numList.Add(21);
+            numList.Add(45);
+            //create static method AddToList() takes in a string into parameter. method should add the strings grapes,oranges,and string
+            //parameter to a list of strings  example cherries passed on to parameters.. method will return list grapes,oranges,cherries 
+
+
+            //create static method TryMe(int, int,)takes in 2 numbers..method should devide first parameter by the second parameter and return 9
+            //if there is a Arithmatic exception if no error return correct result from opperation.
+
+        }
+
+}
